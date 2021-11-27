5d8c6b9e2bccd4a86654ae24d4a0be6d448ac02e
nilesh-bitcot <nilesh.bitcot@gmail.com>
2021-11-27 16:21:30 +0530

    test changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@ $errors->any() -->
 <div class="row justify-content-center">
     <div class="col-lg-6">
         <div class="card shadow-lg border-0 rounded-lg mt-5">
-            <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+            <div class="card-header"><h3 class="text-center font-weight-light my-4">Login here</h3></div>
             <div class="card-body">  
 				<form action="{{url('post-login')}}" method="POST" id="logForm" class="needs-validation {{ $errors->any() ? 'was-validated' : '' }}" novalidate>
 				{{ csrf_field() }}
